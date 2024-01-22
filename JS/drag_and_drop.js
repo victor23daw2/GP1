5e1f40708f07990613207687c30bb6b0eef8e8d8
romsar23daw2 <15586185.clot@fje.edu>
2024-01-22 12:50:25 +0000

    v.10 - Some small improvements

---
+++ JS/drag_and_drop.js
@@ -0,0 +1,81 @@
+window.onload = function () {
+  var squares = document.querySelectorAll(".square");
+  var dragContainer = document.querySelector(".drag");
+
+  squares.forEach(function (square) {
+    square.addEventListener("dragenter", gestionarEntradaDrag, false);
+    square.addEventListener("dragleave", gestionarSalirDrag, false);
+    square.addEventListener("dragover", gestionarSobreDrag, false);
+    square.addEventListener("drop", gestionarDropSquare, false);
+  });
+
+  dragContainer.addEventListener("dragover", gestionarSobreDrag, false);
+  dragContainer.addEventListener("drop", gestionarDropContainer, false);
+
+  var imatges = document.querySelectorAll(".drag img");
+  imatges.forEach(function (item) {
+    item.addEventListener("dragstart", gestionarIniciDrag, false);
+  });
+
+  function gestionarEntradaDrag(ev) {
+    ev.preventDefault();
+    // Cambia el color de fondo solo si el elemento tiene la clase "square".
+    if (ev.target.classList.contains("square")) {
+      // Verifica en qué columna se encuentra el cuadro de destino
+      var isLeftColumn = ev.target.closest(".container-left");
+      // Cambia el color en consecuencia
+      ev.target.style.backgroundColor = isLeftColumn ? "lightblue" : "lightcoral";
+    }
+  }
+
+  function gestionarSalirDrag(ev) {
+    // Restaura el color de fondo solo si el elemento tiene la clase "square".
+    if (ev.target.classList.contains("square")) {
+      ev.target.style.backgroundColor = "";
+    }
+  }
+
+  function gestionarSobreDrag(ev) {
+    ev.preventDefault();
+  }
+
+  function gestionarIniciDrag(ev) {
+    ev.dataTransfer.setData("imatge", ev.target.id);
+  }
+
+  function gestionarDropSquare(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("imatge");
+
+    // Verifica si el contenedor es un square y no tiene una imagen
+    if (ev.target.classList.contains("square") && !ev.target.querySelector("img")) {
+      // Mueve la imagen al contenedor
+      var draggedElement = document.getElementById(data);
+      ev.target.appendChild(draggedElement);
+
+      // Mantén el color de fondo del square después de soltar la imagen
+      var isLeftColumn = ev.target.closest(".container-left");
+      ev.target.style.backgroundColor = isLeftColumn ? "lightblue" : "lightcoral";
+
+      // Reproduce un sonido
+      var audio = new Audio('./Assets/Audio/gota.mp3');
+      audio.play();
+    }
+  }
+
+  function gestionarDropContainer(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("imatge");
+
+    // Mueve la imagen al contenedor .drag
+    var draggedElement = document.getElementById(data);
+    dragContainer.appendChild(draggedElement);
+
+    // Restaura el color de fondo del square
+    draggedElement.closest(".square").style.backgroundColor = "";
+
+    // Reproduce un sonido
+    var audio = new Audio('./Assets/Audio/gota.mp3');
+    audio.play();
+  }
+};
---
+++ servidor-MongoDB.js
@@ -0,0 +1,87 @@
+let MongoClient = require("mongodb").MongoClient;
+let assert = require("assert"); //utilitzem assercions
+
+function iniciarMongo() {
+  let ruta = "mongodb://localhost:27017/projecteDAW2";
+
+  MongoClient.connect(ruta, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connexió correcta");
+    let db = client.db("projecteDAW2"); // Here I say the database that I'll use.
+
+    // This function adds different users.
+    // afegirClients(db, err, function () {});
+
+    // This function shows the information of the user with sergi as name.
+    consultarDocument(db, err, function () {});
+
+    // This function deletes all of the users of the database.
+    // esborrarTotsDocuments(db, err, function () {
+    //   client.close();
+    // });
+
+    // db.close();
+  });
+}
+
+let afegirClients = function (db, err, callback) {
+  db.collection("usuaris").insertOne({
+    nom: "Peta",
+    cognom: "Zetta",
+    edat: 25,
+    correu: "petazetta123@proton.me",
+    telf: "123-567-890",
+    tipus: "client",
+  });
+
+  db.collection("usuaris").insertOne({
+    nom: "Pedro",
+    cognom: "Picapiedra",
+    edat: 54,
+    correu: "pedroelpicapiedra@gmail.com",
+    telf: "456-123-970",
+    tipus: "client",
+  });
+
+  db.collection("usuaris").insertOne({
+    nom: "Ramon",
+    cognom: "Carbon",
+    edat: 45,
+    correu: "ramoncarbon@gmail.com",
+    telf: "915-432-760",
+    tipus: "client",
+  });
+
+  assert.equal(err, null);
+  console.log("Creant diversos clients a col·lecció usuaris");
+  callback();
+};
+
+let consultarDocument = function (db, err, callback) {
+  let cursor = db.collection("usuaris").find({
+    nom: "Pedro", // Here I search for the user that has the name "sergi".
+  });
+
+  cursor.toArray(function (err, results) {
+    assert.equal(err, null);
+
+    if (results != null) {
+      results.forEach((doc) => {
+        console.log(`Usuari: ${doc.nom}, correu: ${doc.correu}`);
+      });
+    } else {
+      callback();
+    }
+  });
+};
+
+let esborrarTotsDocuments = function (db, err, callback) {
+  console.log(`Esborrant tots els usuaris de la base de dades.`);
+
+  db.collection("usuaris").deleteMany({}, function (err, results) {
+    //console.log(results);
+    callback();
+  });
+};
+
+exports.iniciarMongo = iniciarMongo;
